54facb8efb1f4288118d234a72584caaa7801e98
marianasampaiodev <marianags4mpaio@gmail.com>
2026-06-05 11:08:27 -0300

    Refactor: adiconando categoria

--- src/app/core/services/category.ts
+++ src/app/core/services/category.ts
@@ -22,6 +22,7 @@ export class CategoryService {
     { name: 'Educação', color: '#FFE66D', type: 'expense' },
     { name: 'Lazer', color: '#FF85A2', type: 'expense' },
     { name: 'Compras', color: '#B19CD9', type: 'expense' },
+    { name: 'Fixas', color: '#c4db3d', type: 'expense' },
     { name: 'Outros', color: '#A5D6A7', type: 'expense' },
     // Receitas
     { name: 'Salário', color: '#6BCB77', type: 'income' },
